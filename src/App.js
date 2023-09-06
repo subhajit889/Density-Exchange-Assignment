e40852bf1b11744cc77100f6289877408794bedd
Subhajit Das <apctocla@gmail.com>
2023-09-06 12:52:53 +0530

    initialize with new react project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div className="bg-slate-600">
+      <h1 className='px-4'>this is navbar</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
